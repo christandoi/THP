40911a20448da4aa5731c18556d67723566f778f
christandoi <christandoi@gmail.com>
2017-01-17 16:07:38 -0500

    Add files via upload

--- CollectAndStoreTHP.py
+++ CollectAndStoreTHP.py
@@ -9,25 +9,27 @@ con = sqlite3.connect(env_logger_config.dbfile)
 sensor = Adafruit_DHT.AM2302
 
 insert_sql = """INSERT INTO thp (datetime, temperature, humidity, pressure) VALUES (?,?,?,?) """
-		
-while True:
 	
-	now = datetime.datetime.now()	
-	formtime = str(now.strftime('%Y-%m-%d %H:%M:%S'))
-	bmp = bmp183()
-
-	with con:
-	   	humidity, temperature = Adafruit_DHT.read_retry(sensor, env_logger_config.gpio_port)
-		bmp.measure_pressure()
-		pressure = float(bmp.pressure)
-		print 
-		cur = con.cursor()
-		data =  [formtime,temperature,humidity,pressure]
-		cur.execute(insert_sql, data)
-	con.commit()
+try:
+	
+	while True:
 		
-	print "Commit: %s %.1f %.1f %.1f" % tuple(data)
-	time.sleep(env_logger_config.log_interval)
+		now = datetime.datetime.now()	
+		formtime = str(now.strftime('%Y-%m-%d %H:%M:%S'))
+		bmp = bmp183()
+
+		with con:
+		   	humidity, temperature = Adafruit_DHT.read_retry(sensor, env_logger_config.gpio_port)
+			bmp.measure_pressure()
+			pressure = float(bmp.pressure)
+			print 
+			cur = con.cursor()
+			data =  [formtime,temperature,humidity,pressure]
+			cur.execute(insert_sql, data)
+		con.commit()
+			
+		print "Commit: %s %.1f %.1f %.1f" % tuple(data)
+		time.sleep(env_logger_config.log_interval)
 
 finally:
 	
