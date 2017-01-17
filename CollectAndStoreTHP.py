fec8169262462d16eb4b143f04d7a506ede92c3b
christandoi <christandoi@gmail.com>
2017-01-17 16:10:48 -0500

    Add files via upload

--- CollectAndStoreTHP.py
+++ CollectAndStoreTHP.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys, Adafruit_DHT, os, os.path, time, datetime, sqlite3
+import RPi.GPIO as GPIO
 from bmp183 import bmp183
 import env_logger_config
 
 con = sqlite3.connect(env_logger_config.dbfile)
 sensor = Adafruit_DHT.AM2302
+bmp = bmp183()
 
 insert_sql = """INSERT INTO thp (datetime, temperature, humidity, pressure) VALUES (?,?,?,?) """
 	
@@ -16,13 +18,11 @@ try:
 		
 		now = datetime.datetime.now()	
 		formtime = str(now.strftime('%Y-%m-%d %H:%M:%S'))
-		bmp = bmp183()
 
 		with con:
 		   	humidity, temperature = Adafruit_DHT.read_retry(sensor, env_logger_config.gpio_port)
 			bmp.measure_pressure()
 			pressure = float(bmp.pressure)
-			print 
 			cur = con.cursor()
 			data =  [formtime,temperature,humidity,pressure]
 			cur.execute(insert_sql, data)
